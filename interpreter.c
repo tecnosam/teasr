2d67288a6d63c6979e4b6893b92437b44e94c804
Samuel Abolo <ikabolo59@gmail.com>
2025-06-29 02:53:36 +0100

    Command line argument instead

--- interpreter.c
+++ interpreter.c
@@ -1,48 +1,38 @@
-
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "lib/loader.h"
 #include "lib/executor.h"
 
-
 long get_file_size(FILE *file) {
-
     if (file == NULL) {
         perror("File Size: Error opening file");
         return -1;
     }
-    // Move to end of file to get size
+
     fseek(file, 0, SEEK_END);
     long filesize = ftell(file);
-    rewind(file);  // Go back to the beginning
-
+    rewind(file);
     return filesize;
 }
 
-
 int load_file(FILE *file, unsigned char *buffer, long filesize) {
-
-    // Read into buffer
     size_t bytesRead = fread(buffer, 1, filesize, file);
     if (bytesRead != filesize) {
         perror("Error reading file");
-        free(buffer);
-        fclose(file);
         return 1;
     }
-
     return 0;
 }
 
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
 
+    const char *filename = argv[1];
 
-int main() {
-
-    char filename[100];
-    printf("Enter filename: ");
-    scanf("%99s", filename);
-
-    // Open in binary mode
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
         perror("Error opening file");
@@ -51,34 +41,35 @@ int main() {
 
     long filesize = get_file_size(file);
     if (filesize < 0) {
-        perror("Could not get file size!");
+        fclose(file);
         return 1;
     }
 
-    // Allocate buffer
     unsigned char *buffer = malloc(filesize);
     if (buffer == NULL) {
         perror("Memory allocation failed");
+        fclose(file);
         return 1;
     }
 
-    load_file(file, buffer, filesize);
+    if (load_file(file, buffer, filesize) != 0) {
+        free(buffer);
+        fclose(file);
+        return 1;
+    }
 
-    // close file
     fclose(file);
 
-    // // Example usage: print as hex
     for (long i = 0; i < filesize; ++i) {
         printf("%02x ", buffer[i]);
     }
-    Instruction * instruction_set = get_instruction_set(buffer, filesize);
+
+    Instruction *instruction_set = get_instruction_set(buffer, filesize);
     printf("\nNumber of instructions: %d\n", count_instructions(instruction_set));
 
-    Instruction * iterator;
-    iterator = instruction_set;
-
+    Instruction *iterator = instruction_set;
     while (iterator != NULL) {
-        printf("%c executed %d times\n", (char)iterator->instruction_type, (int) iterator->count);
+        printf("%c executed %d times\n", (char)iterator->instruction_type, (int)iterator->count);
         iterator = iterator->next_instruction;
     }
 
@@ -88,5 +79,4 @@ int main() {
 
     free(buffer);
     return 0;
-
 }
